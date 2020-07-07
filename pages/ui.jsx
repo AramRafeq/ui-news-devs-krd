db7f118a2437eb5f2b9e3d19b5a992ded0749ba1
aram rafeq <aramrafeq2@gmail.com>
2020-07-08 00:42:26 +0300

    going strong step 1

--- pages/ui.jsx
+++ pages/ui.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {
   Row, Col, Avatar, Popover, Button, Input,
-  Carousel,
+  Carousel, Typography,
 } from 'antd';
 import {
   UserOutlined, SettingOutlined, ExportOutlined, SearchOutlined,
@@ -45,18 +45,22 @@ class UI extends React.Component {
       {
         img: 'https://www.sentry.dev/_assets2/static/index-header-lg-6a2edfb1d9841893801fd45ce890f904.jpg',
         title: 'هەڵەی کۆدەکانت بدۆزەوە بە باکارهێنانی sentry.io',
+        desc: 'سڵاو لەپاش دابڕانێکی هێجگار زۆر، من سامڕەند حاجی سەرنوسەری ماڵپەری درەختی گەشەپێدەران و لێکۆڵەرەوەی بواری ئاسایشی زانیاریەکان لەگاڵتاندام بۆ زنجیرەیەک بابەت کە تایبەتە بە پاراستنی زانیاریەکان هەرچەندە چەندین بابەتی سەربەخۆ لەسەر پاراستنی زانیاریەکان لە ماڵپەرەکەماندا هەیە، بەڵام دەمانەوەێت هەمووی لە چەند زنجیرەیەکدا باس بکەینەوە. لە ماڵپەرەکەمانەوە بینەربە.',
       },
       {
         title: 'مانای کۆد نوسین بزانە بۆ ئەوەی ببیت بە کەسێکی سەرکەوتوو',
         img: 'https://devstree.io/wp-content/uploads/2019/10/1_zTdZMxbTkVdXCOoZlXLnsg.png',
+        desc: 'وەك ئاگادارین فلەتەر ئێستا بۆتە یەكەێك لە بەناوبانگترین تەكنەلۆجیای دروست كردنی ئاپی مۆبایل و ئێمە ڵەم بابەتەیا فێرتان ئەكەین چۆن لەسەر كۆمپیوتەرەكانمان جێگیری بكەین و كاری پێبكەین.',
       },
       {
         title: 'فڵەتەر فێرببە دامەزراندنی بۆ یەکەم جار',
         img: 'https://codete.com/blog/wp-content/uploads/2018/07/cover-flutter-blog.jpg',
+        desc: 'ئێمە چەند پڕۆژەیەکەمان هەیە و پڕۆژەکانیش هەمووی سەرچاوەکراوەن. بۆ نموونە ناوی کوردی و پێرمادیلیت. بەڵام ئەوجارە پلانمان وایە کە جگە لەوەی کۆدەکەی سەرچاوەکراوەبێت، تەواوی پڕۆسەی درووست کردنەکەشی ڕیکۆرد بکەین و لە وێبسایتی درەختی گەشەپێدەران بڵاویکەینەوە.',
       },
       {
         title: 'پلەیس هۆڵدەرەکە',
         img: 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png',
+        desc: 'هیچ كاتێك هه‌بووه‌ پرۆگرامێك نوسیوه‌و له‌ سه‌ر سێرڤه‌ر كێشه‌ی هه‌بووبێت و نه‌تزانیبێت چیه‌ یان كاتێك له‌سه‌ر كۆمپیته‌ری كه‌سێك هه‌ڵه‌یه‌ك رووده‌ده‌ات نازانیت بۆچی ئه‌مه‌ رویداوه‌‌ زۆر به‌سوود ده‌بێت ئه‌گه‌ر بێتو هه‌ڵه‌یه‌ك رویدات یه‌كسه‌ر پێیبزانیت رێك وه‌ك ئه‌وه‌ی خۆت كۆده‌كه‌ت debug بكه‌یت، له‌ به‌ختی ئێمه‌ sentry.io  رێك ئه‌و كاره‌ ده‌كات',
       },
     ];
 
@@ -138,19 +142,30 @@ class UI extends React.Component {
                     position: 'relative',
                   }}
                   />
-                  {/* <h2 style={{
+                  <h2 style={{
                     textAlign: 'right',
                     position: 'relative',
-                    background: 'red',
-                    // marginTop: -170,
+                    marginTop: -70,
                     zIndex: 3,
                     color: 'white',
-                    padding: 10,
+                    padding: 20,
                     bottom: 150,
                   }}
                   >
                     {d.title}
-                  </h2> */}
+                  </h2>
+                  <p style={{
+                    position: 'relative',
+                    top: -180,
+                    zIndex: 33,
+                    padding: 20,
+                    color: 'white',
+                    fontSize: 18,
+                    textAlign: 'right',
+                  }}
+                  >
+                    {d.desc}
+                  </p>
                 </div>
               ))}
             </Carousel>
