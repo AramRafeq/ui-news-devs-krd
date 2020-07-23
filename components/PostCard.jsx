465699d47392cc89b88de4af3f7f40ac8c4e910a
aram <aramrafeq2@gmail.com>
2020-07-23 10:53:08 +0300

    post card changed

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,15 +1,45 @@
 import React from 'react';
-import { Card } from 'antd';
+import { Card, message } from 'antd';
+import {
+  FireOutlined,
+  CopyOutlined,
+  FacebookOutlined,
+  TwitterOutlined,
+
+} from '@ant-design/icons';
 
 class Slide extends React.Component {
+  constructor(props) {
+    super(props);
+    this.copyUrl = (url) => {
+      navigator.clipboard.writeText(url);
+      message.success('بەستەر کۆپی کرا');
+    };
+  }
+
   render() {
     const { data } = this.props;
     return (
       <Card
         cover={<div style={{ padding: 6 }}><img style={{ width: '100%', borderRadius: 7 }} alt="test" src={data.img} /></div>}
         headStyle={{ background: 'red' }}
+        actions={[
+          <div className="post-card-fav">
+            <span>200</span>
+            {' '}
+            <FireOutlined key="fav" />
+          </div>,
+          <TwitterOutlined key="twitter" />,
+          <FacebookOutlined />,
+          <CopyOutlined onClick={() => this.copyUrl(data.url)} />,
+        ]}
       >
-        <Card.Meta title={data.title} description={data.desc} />
+        <Card.Meta
+          title={
+            <a href={data.url} alt={data.title}>{data.title}</a>
+          }
+          description={data.desc}
+        />
       </Card>
     );
   }
