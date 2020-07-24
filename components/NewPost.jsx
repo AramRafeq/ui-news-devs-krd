b4411b0c1221663b66f9cad8410ae5cf6409d969
aramrafeq <aramrafeq2@gmail.com>
2020-07-25 00:49:36 +0300

    new post implemnted

--- components/NewPost.jsx
+++ components/NewPost.jsx
@@ -1,40 +1,121 @@
+/* eslint-disable camelcase */
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import moment from 'moment';
 import { observer, inject } from 'mobx-react';
 
 import {
-  Form, Input, Button, Row, Col,
+  Form, Input, Button, Row, Col, DatePicker, notification,
 } from 'antd';
+
+import superagent from '../helpers/superagent';
 import Base64Uploader from './basic/Base64Uploader';
 import AuthGuard from './AuthGuard';
 
-moment.locale('ku');
+// moment.locale('ku');
 
 @inject('userStore', 'tokenStore')
 @observer
 class NewPost extends React.Component {
+  constructor(props) {
+    super(props);
+    this.initialState = () => ({
+      saving: false,
+      thumbnail: '',
+      original_thumbnail: '',
+    });
+    this.state = this.initialState();
+    const token = props.tokenStore.value;
+    superagent.set('authorization', `Bearer ${token}`);
+
+    this.onFinish = (values) => {
+      this.setState({ saving: true });
+      const postObject = {
+        ...values,
+        post_date: moment(values.post_date).locale('en').format('YYYY-MM-DD').toString(),
+      };
+      const { thumbnail, original_thumbnail } = this.state;
+      if (thumbnail !== '') {
+        postObject.thumbnail = thumbnail;
+        postObject.original_thumbnail = original_thumbnail;
+      }
+      superagent.post('/link')
+        .send(postObject).end((err, info) => {
+          this.setState({ saving: false });
+          if (!err) {
+            this.setState({ thumbnail: '' });
+            if (this.form.current) {
+              this.form.current.resetFields();
+            }
+            notification.success({
+              message: 'سەرکەوتوبوو',
+              description: 'به‌سته‌ری نوێ بڵاوكرایه‌وه‌',
+              placement: 'bottomRight',
+            });
+          }
+        });
+    };
+    this.form = React.createRef();
+  }
+
   render() {
+    const { saving } = this.state;
     return (
       <AuthGuard>
         {' '}
-        <Form layout="vertical">
+        <Form layout="vertical" onFinish={this.onFinish} ref={this.form}>
           <Row gutter={(10)}>
-            <Col span={24}>
-              <Form.Item label="سه‌ردێر">
+            <Col span={18}>
+              <Form.Item
+                label="سه‌ردێر"
+                name="title"
+                rules={[
+                  {
+                    required: true,
+                    message: 'سه‌ردێری بابه‌ت پێویسته‌',
+                  },
+                  {
+                    type: 'string',
+                    min: 2,
+                    message: 'سه‌ردێری بابه‌ت زۆر كورته‌',
+                  },
+                ]}
+              >
                 <Input style={{ borderRadius: 6 }} />
               </Form.Item>
             </Col>
-          </Row>
-          <Row>
-            <Col span={24}>
-              <Form.Item label="كورته‌ی بابه‌ت">
-                <Input.TextArea rows={4} style={{ borderRadius: 6 }} />
+            <Col span={6}>
+              <Form.Item
+                label="به‌رواری  بابه‌ت"
+                name="post_date"
+                rules={[
+                  {
+                    required: true,
+                    message: 'به‌رواری نوسینی بابه‌ت پێویسته‌',
+                  },
+                ]}
+              >
+                <DatePicker placeholder="به‌روار هه‌ڵبژێره‌" style={{ borderRadius: 6, width: '100%' }} />
               </Form.Item>
             </Col>
           </Row>
+
           <Row gutter={[10, 10]}>
             <Col span={18}>
-              <Form.Item label="به‌سته‌ر">
+              <Form.Item
+                label="به‌سته‌ر"
+                name="url"
+                rules={[
+                  {
+                    required: true,
+                    message: 'به‌سته‌ری بابه‌ت پێویسته‌',
+                  },
+                  {
+                    type: 'url',
+                    message: 'به‌سته‌ری بابه‌ت هه‌ڵه‌یه‌‌',
+                  },
+                ]}
+              >
                 <Input style={{ borderRadius: 6 }} />
               </Form.Item>
             </Col>
@@ -42,9 +123,30 @@ class NewPost extends React.Component {
               <Base64Uploader style={{ borderRadius: 7 }} text="وێنه‌یه‌ك هه‌ڵبژێره‌" removeText="بسره‌وه‌" />
             </Col>
           </Row>
+          <Row>
+            <Col span={24}>
+              <Form.Item
+                label="كورته‌ی بابه‌ت"
+                name="desc"
+                rules={[
+                  {
+                    required: true,
+                    message: 'كورته‌ی بابه‌ت پێویسته‌',
+                  },
+                  {
+                    type: 'string',
+                    min: 20,
+                    message: 'كورته‌ی بابه‌ت زۆر كورته‌ پێوسیته‌ له‌ ٢٠ پیت كه‌متر نه‌بێت',
+                  },
+                ]}
+              >
+                <Input.TextArea rows={4} style={{ borderRadius: 6 }} />
+              </Form.Item>
+            </Col>
+          </Row>
           <Row gutter={[10, 10]}>
             <Col span={24}>
-              <Button loading type="primary" style={{ background: '#2b2c34' }}>بڵاوكردنه‌وه‌</Button>
+              <Button loading={saving} type="primary" htmlType="submit" style={{ background: '#2b2c34' }}>بڵاوكردنه‌وه‌</Button>
             </Col>
           </Row>
         </Form>
